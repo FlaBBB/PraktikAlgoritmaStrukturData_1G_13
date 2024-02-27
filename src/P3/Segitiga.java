2084ad59c68cdf1f02064ad170402192a2123dcc
FlaBBB <f12345ff67@gmail.com>
2024-02-27 20:58:34 +0700

    P3_Percobaan 3 - Tugas 4_13

--- src/P3/Segitiga.java
+++ src/P3/Segitiga.java
@@ -8,4 +8,12 @@ public class Segitiga {
         alas = a;
         tinggi = t; 
     }
+
+    public double hitungLuas() {
+        return 0.5 * alas * tinggi;
+    }
+
+    public double hitungKeliling() {
+        return alas + tinggi + Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+    }
 }
