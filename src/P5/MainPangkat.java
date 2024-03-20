255df4f099b0ae933f1d3921fbf0e18f83b631b7
FlaBBB <f12345ff67@gmail.com>
2024-03-20 09:20:31 +0700

    P5_Percobaan 2 - Tugas 4_13

--- src/P5/MainPangkat.java
+++ src/P5/MainPangkat.java
@@ -12,6 +12,10 @@ public class MainPangkat {
         System.out.println("-----------------------------");
         System.out.println("Masukkan jumlah elemen yang ingin dihitung: ");
         int iJml = sc.nextInt();
+        System.out.println("Pilih metode perhitungan: ");
+        System.out.println("1. Brute Force");
+        System.out.println("2. Divide Conquer");
+        int iMetode = sc.nextInt();
 
         Pangkat[] fk = new Pangkat[iJml];
         for (int i = 0; i < iJml; i++) {
@@ -22,14 +26,26 @@ public class MainPangkat {
             fk[i] = new Pangkat(iNilai, iPangkat);
         }
 
-        System.out.println("HASIL- BRUTE FORCE");
-        for (int i = 0; i < iJml; i++) {
-            System.out.println("Hasil dari " + fk[i].nilai + " pangkat " + fk[i].pangkat + " adalah " + fk[i].pangkatBF());
+        switch (iMetode) {
+            case 1:
+                System.out.println("HASIL- BRUTE FORCE");
+                for (int i = 0; i < iJml; i++) {
+                    System.out.println(
+                            "Hasil dari " + fk[i].nilai + " pangkat " + fk[i].pangkat + " adalah " + fk[i].pangkatBF());
+                }
+                break;
+            case 2:
+
+                System.out.println("HASIL- DIVIDE CONQUER");
+                for (int i = 0; i < iJml; i++) {
+                    System.out.println(
+                            "Hasil dari " + fk[i].nilai + " pangkat " + fk[i].pangkat + " adalah " + fk[i].pangkatDC());
+                }
+                break;
+            default:
+                System.out.println("Pilihan tidak tersedia");
+                break;
         }
 
-        System.out.println("HASIL- DIVIDE CONQUER");
-        for (int i = 0; i < iJml; i++) {
-            System.out.println("Hasil dari " + fk[i].nilai + " pangkat " + fk[i].pangkat + " adalah " + fk[i].pangkatDC());
-        }
     }
 }
