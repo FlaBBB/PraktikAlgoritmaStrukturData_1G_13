ba6d89987c9ba6a2836d2fb109284918feb725cd
FlaBBB <f12345ff67@gmail.com>
2024-06-04 10:35:35 +0700

    P14_Percobaan 2_13

--- src/P14/GraphMain13.java
+++ src/P14/GraphMain13.java
@@ -27,5 +27,19 @@ public class GraphMain13 {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        GraphMatriks13 gdg = new GraphMatriks13(4);
+        gdg.makeEdge(0, 1, 5);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90); 
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
+
+        gdg.degree();   
     }
 }
